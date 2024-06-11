ea5723e7462679bc3f67b214c4c2e40acd73ae26
yovizn <yovizulkarnaen@gmial.com>
2024-06-11 09:33:41 +0700

    feat(api/transactions): transactions initialize

--- api/src/app/services/event.service.ts
+++ api/src/app/services/event.service.ts
@@ -82,6 +82,12 @@ class EventServices {
         if (findTitle?.title)
             throw new Error('Title is used, try another title')
 
+        if (Number(priceVip) < 1000)
+            throw new Error('VIP cannot less then 1000')
+
+        if (priceVip && Number(capacityVip) < 0)
+            throw new Error('At least capacity need more than 0')
+
         return await prisma.$transaction(async (prisma) => {
             const generateSlug = `${toSlug(title.trim())}-${nanoid(10)}`
             const data: Prisma.EventCreateInput = {
@@ -130,7 +136,7 @@ class EventServices {
                 data: dataTicket,
             })
 
-            if (priceVip && capacityVip) {
+            if (priceVip && capacityVip && Number(priceVip) > 1000) {
                 const dataTicket: Prisma.TicketsCreateInput = {
                     id: nanoid(),
                     type: 'VIP',
@@ -146,173 +152,6 @@ class EventServices {
 
             return { title: event.title }
         })
-
-        // return createEvent.id
-
-        // const eventCreate = await prisma.$transaction(async (prisma) => {
-        //     const generateSlug = `${toSlug(title.trim())}-${nanoid(10)}`
-        //     const eventId = nanoid()
-        //     const imageId = nanoid()
-        //     const isTitleExist = await prisma.event.findFirst({
-        //         where: { title },
-        //         select: { title: true },
-        //     })
-        //     if (isTitleExist?.title) throw new Error('Title already exist')
-        //     const data: Prisma.EventCreateInput = {
-        //         id: eventId,
-        //         slug: generateSlug,
-        //         title,
-        //         startAt,
-        //         endAt,
-        //         location,
-        //         description,
-        //         city,
-        //         venueType,
-        //         category,
-        //         use_voucher: useVoucher === 'true' ? true : false,
-        //         promotor: { connect: { id: req.user?.Promotor?.id } },
-        //         // eventImage: { connect: { id: imageId } },
-        //     }
-
-        //     const event = await prisma.event.create({
-        //         data,
-        //     })
-
-        //     if (file) {
-        //         const blob = await sharp(file.buffer).webp().toBuffer()
-        //         const slug = `${toSlug(file.fieldname)}-${nanoid(10)}`
-        //         const name = `promotor_poster/${slug}`
-        //         const image: Prisma.ImageCreateInput = {
-        //             id: imageId,
-        //             blob,
-        //             name,
-        //         }
-        //         await prisma.image.create({
-        //             data: image,
-        //         })
-        //     }
-
-        //     const dataTicketsReguler: Prisma.ticketsCreateInput = {
-        //         id: nanoid(),
-        //         capacity: capacityReguler,
-        //         events: { connect: { id: eventCreate.id } },
-        //         price: !priceReguler ? 0 : priceReguler,
-        //         ticketType: 'reguler',
-        //     }
-        //     if (priceVip && capacityVip) {
-        //         const dataTicketsVip: Prisma.ticketsCreateInput = {
-        //             id: nanoid(),
-        //             capacity: capacityVip,
-        //             events: { connect: { id: eventCreate.id } },
-        //             price: !priceVip ? 0 : priceVip,
-        //             ticketType: 'vip',
-        //         }
-
-        //         await prisma.tickets.create({
-        //             data: dataTicketsVip,
-        //         })
-        //     }
-        //     await prisma.tickets.create({
-        //         data: dataTicketsReguler,
-        //     })
-        //     return { id: event.id }
-        // })
-
-        // await prisma.$transaction(async (prisma) => {
-
-        // })
-
-        // return eventCreate.id
-
-        // const isTitleExist = await prisma.event.findFirst({ where: { title } })
-        // if (isTitleExist) throw new Error('title already exist')
-
-        // const generateSlug = `${toSlug(title.trim())}-${nanoid(10)}`
-
-        // const eventId = nanoid()
-        // const imageId = nanoid()
-        // const data: Prisma.EventCreateInput = {
-        //     id: eventId,
-        //     slug: generateSlug,
-        //     title,
-        //     startAt,
-        //     endAt,
-        //     city,
-        //     location,
-        //     description,
-        //     venueType,
-        //     category,
-        //     eventImage: { connect: { id: imageId } },
-        //     use_voucher: useVoucher,
-        //     promotor: { connect: { id: req.user?.Promotor!.id } },
-        // }
-        // if (file) {
-        //     const blob = await sharp(file.buffer).webp().toBuffer()
-        //     const slug = `${toSlug(file.fieldname)}-${nanoid(10)}`
-        //     const name = `promotor_poster/${slug}`
-
-        //     const image: Prisma.ImageCreateInput = {
-        //         id: imageId,
-        //         blob,
-        //         name,
-        //     }
-        //     await prisma.image.create({
-        //         data: image,
-        //     })
-        // }
-        // const ticketRegulerData: Prisma.ticketsCreateInput = {
-        //     id: nanoid(),
-        //     events: { connect: { id: eventId } },
-        //     ticketType: 'reguler',
-        //     price: priceReguler,
-        //     capacity: capacityReguler,
-        // }
-        // const ticketVipData: Prisma.ticketsCreateInput = {
-        //     id: nanoid(),
-        //     events: { connect: { id: eventId } },
-        //     ticketType: 'vip',
-        //     price: priceVip,
-        //     capacity: capacityVip,
-        // }
-
-        // const transaction_ops: any[] = [prisma.event.create({ data })]
-        // if (capacityReguler && capacityReguler > 0)
-        //     transaction_ops.push(
-        //         prisma.tickets.upsert({
-        //             create: ticketRegulerData,
-        //             where: {
-        //                 eventId_ticketType: {
-        //                     eventId: eventId,
-        //                     ticketType: 'reguler',
-        //                 },
-        //             },
-        //             update: {
-        //                 price: ticketRegulerData.price,
-        //                 capacity: ticketRegulerData.capacity,
-        //             },
-        //         })
-        //     )
-
-        // if (capacityVip && capacityVip > 0)
-        //     transaction_ops.push(
-        //         prisma.tickets.upsert({
-        //             create: ticketVipData,
-        //             where: {
-        //                 eventId_ticketType: {
-        //                     eventId: eventId,
-        //                     ticketType: 'vip',
-        //                 },
-        //             },
-        //             update: {
-        //                 price: ticketVipData.price,
-        //                 capacity: ticketVipData.capacity,
-        //             },
-        //         })
-        //     )
-
-        // await prisma.$transaction(transaction_ops)
-
-        // return eventId
     }
 
     async editEvent(req: Request) {
--- api/src/app/services/transaction.service.ts
+++ api/src/app/services/transaction.service.ts
@@ -1,19 +1,99 @@
-import type { Request } from "express";
-import { prisma } from "../../libs/prisma";
+import type { Request } from 'express'
+import { prisma } from '../../libs/prisma'
+import { Prisma } from '@prisma/client'
+import { nanoid } from 'nanoid'
+import { generateInvoice } from '../../utils/generate'
 
 class TransactionService {
-  async updateTransactionStatus(
-    req: Request,
-    userId: string,
-    transactionId: string
-  ) {
-    const { status } = req.body;
+    async updateTransactionStatus(
+        req: Request,
+        userId: string,
+        transactionId: string
+    ) {
+        const { status } = req.body
 
-    await prisma.transaction.update({
-      data: { status },
-      where: { userId, id: transactionId },
-    });
-  }
+        await prisma.transaction.update({
+            data: { status },
+            where: { userId, id: transactionId },
+        })
+    }
+
+    async createTransaction(req: Request) {
+        const { ticketId } = req.params
+        const { totalItems, usePoint, useVoucher } = req.body
+        const findTicket = await prisma.tickets.findFirst({
+            where: { id: ticketId },
+            include: { event: true },
+        })
+
+        if (!findTicket?.id) throw new Error('Cannot find this event')
+        if (totalItems < 1) throw new Error('Invalid Total Items')
+        if (usePoint && !req.user?.point)
+            throw new Error('Cannot use point balance is not enough')
+
+        return await prisma.$transaction(async (tx) => {
+            const voucher = await tx.voucher.findFirst({
+                where: { userId: req.user?.id },
+            })
+            const invoiceNumber = generateInvoice(findTicket.event.id)
+            const discountPoint = Number(req.user?.point)
+            const totalSum = new Prisma.Decimal(findTicket.price)
+                .minus(discountPoint)
+                .mul(Number(totalItems))
+
+            const total =
+                useVoucher == 'true'
+                    ? totalSum.mul(new Prisma.Decimal(0.9))
+                    : totalSum
+
+            const data: Prisma.TransactionCreateInput = {
+                id: nanoid(),
+                invoiceNumber,
+                event: { connect: { id: findTicket.event.id } },
+                user: { connect: { id: req.user?.id } },
+                total,
+                discountPoint: usePoint == 'true' ? discountPoint : 0,
+                Voucher:
+                    useVoucher == 'true'
+                        ? { connect: { id: voucher?.id } }
+                        : undefined,
+            }
+
+            if (usePoint) {
+                await tx.user.update({
+                    where: { id: req.user?.id },
+                    data: { point: 0 },
+                })
+            }
+
+            if (useVoucher) {
+                if (voucher?.isValid === false)
+                    throw new Error('You cannot use voucher')
+
+                await tx.voucher.update({
+                    where: { id: voucher?.id },
+                    data: { isValid: false },
+                })
+            }
+
+            const createTransaction = await tx.transaction.create({
+                data,
+            })
+
+            const dataItem: Prisma.TransactionItemCreateInput = {
+                price: findTicket.price,
+                ticket: { connect: { id: findTicket.id } },
+                transaction: { connect: { id: createTransaction.id } },
+                quantity: Number(totalItems),
+            }
+
+            await tx.transactionItem.create({
+                data: dataItem,
+            })
+
+            return createTransaction
+        })
+    }
 }
 
-export default new TransactionService();
+export default new TransactionService()
